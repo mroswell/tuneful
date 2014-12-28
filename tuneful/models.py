64f7817bbe4980f69f5921cbbcc640a0d0e7d301
Margie Roswell <mroswell@gmail.com>
2014-12-28 18:47:52 -0500

    add models

--- tuneful/models.py
+++ tuneful/models.py
@@ -1,3 +1,4 @@
+
 import os.path
 
 from flask import url_for
@@ -7,3 +8,52 @@ from sqlalchemy.orm import relationship
 from tuneful import app
 from database import Base, engine
 
+
+# The Song model: This should have an integer id column, and a column specifying a one-to-one relationship with a File.
+# The File model: This should have an integer id column, a string column for the file name, and the backref from the one-to-one relationship with the Song.
+
+# {
+#     "id": 1,
+#     "file": {
+#         "id": 7,
+#         "name": "Shady_Grove.mp3"
+#     }
+# }
+
+
+# The File.as_dictionary method should just return the file element of the song dictionary (i.e. {"id": 7, "name": "Shady_Grove.mp3"}).
+
+
+class Song(Base):
+    __tablename__ = "songs"
+
+    id = Column(Integer, primary_key=True)
+
+    file_id = relationship("File", uselist=False, backref="song")
+
+    def as_dictionary(self):
+        song = {
+            "id": self.id,
+            "file": {
+                "id": self.file_id,
+                "body": self.file_id.name
+            }
+        }
+        return song
+
+class File(Base):
+    __tablename__ = "files"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+
+    song_id = Column(Integer, ForeignKey('songs.id'), nullable=False)
+
+    def as_dictionary(self):
+        file = {
+            "file": {
+                "id": self.id,
+                "body": self.name
+            }
+        }
+        return file
